b759f7ebe2dd7c354c1d84131063cb0b81c918d1
Alfred <amothapo@retrorabbit.co.za>
2024-06-17 17:48:13 +0200

    Added new project structure to separate business logic, data access layer and the presentation layer

--- TaskManagement.API/TaskManagement.API/Program.cs
+++ TaskManagement.API/TaskManagement.API/Program.cs
@@ -1,6 +1,17 @@
+using FluentMigrator.Runner;
+using TaskManagement.DataAccessLayer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<DataContext>();
+// Register FluentMigrator services
+builder.Services.AddFluentMigratorCore()
+    .ConfigureRunner(rb => rb
+        .AddPostgres()
+        .WithGlobalConnectionString(builder.Configuration.GetConnectionString("DefaultConnection"))
+        .ScanIn(typeof(Program).Assembly).For.Migrations())
+    .AddLogging(lb => lb.AddFluentMigratorConsole());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +19,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Apply database migrations automatically on startup
+using (var scope = app.Services.CreateScope())
+{
+    var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+    runner.MigrateUp();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
