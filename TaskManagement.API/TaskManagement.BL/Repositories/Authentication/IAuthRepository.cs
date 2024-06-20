88cd3a6d7a684a9f272733eec30aa9dce1c1fb8b
Alfred <amothapo@retrorabbit.co.za>
2024-06-20 17:56:30 +0200

    Added logic to register and login use including jwt authentication

---
+++ TaskManagement.API/TaskManagement.BL/Repositories/Authentication/IAuthRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.BL.Models;
+
+namespace TaskManagement.BL.Repositories.Authentication
+{
+    public interface IAuthRepository
+    {
+        Task RegisterAsync(UserModel user);
+        Task<UserModel> LoginAsync(string emailAddress, string password);
+        Task DeactivateUserAsync(Guid userId);
+    }
+}
